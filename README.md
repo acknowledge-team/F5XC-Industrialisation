4491c47a86d403e978e0495fa9571fbf2ac1f39f
guillADZ <guillaume.houriez@acknowledge.fr>
2023-12-22 01:26:10 +0100

    ADD: un read-me V1 basique, un providers files, des samples utiles pour l'appel des api

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# F5XC-Industrialisation
+<h1 align="left">F5 XC - Terraform - Template - App-Firewall</h1>
+
+###
+<p align="left">
+
+</p>
+
+<p align="left"> Projet : Créer un template de déploiement D'un WAF F5XC.</p>
+
+###
+
+<h2 align="left">Prérequis </h2>
+
+###
+
+<p align="left">Terraform : version 1.6.0 <br>Provider Volterra : version 0.11.26 </p>
+
+###
---
+++ provider.tf
@@ -0,0 +1,17 @@
+# Sample of configuration : https://github.com/cklewar/f5-xc-modules/tree/main/f5xc
+
+terraform {
+  required_providers {
+    volterra = {
+      source = "volterraedge/volterra"
+      version = "0.11.26"
+    }
+  }
+}
+
+provider "volterra" {
+  # Configuration options / set VES_P12_PASSWORD= xxxxx
+  # https://registry.terraform.io/providers/volterraedge/volterra/latest
+   api_p12_file = var.f5_xc_api_p12_file
+   url   = var.f5_xc_api_url
+}
